ded22dbc77a50d5b18e960586083d46d5ed465fc
Andre Soares <andregsilv@gmail.com>
2020-09-20 23:49:23 -0300

    small things

--- lib/raw/scanner.go
+++ lib/raw/scanner.go
@@ -2,10 +2,17 @@ package raw
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 
 	"github.com/stellar/go/crc16"
+)
+
+var (
+	ErrBytesOutOfRange = errors.New("found bytes out of the range 0x20-0x7f")
+	ErrMessageTooShort = errors.New("the message payload is too short. it should be at least 1 in length")
+	ErrMessageTooLong  = errors.New("the message payload is too long. it should be at most 1024 in length")
 )
 
 func PayloadScanner(data []byte, atEOF bool) (int, []byte, error) {
@@ -22,8 +29,14 @@ func PayloadScanner(data []byte, atEOF bool) (int, []byte, error) {
 	if data[0] != SYN {
 		return 0, nil, fmt.Errorf("protocol violation. expecting SYN (0x16) but received %x", data[0])
 	}
-	if i := bytes.IndexByte(data[1:], ETB); i >= 0 {
-		payload := data[1 : i+1]
+	if i := bytes.IndexByte(data, ETB); i >= 0 {
+		if i < 2 {
+			return 0, nil, ErrMessageTooShort
+		}
+		if i > 1024+1 {
+			return 0, nil, ErrMessageTooLong
+		}
+		payload := data[1:i]
 
 		// If data does not have space for CRC
 		totalLength := 1 + len(payload) + 1 + 2
@@ -35,14 +48,14 @@ func PayloadScanner(data []byte, atEOF bool) (int, []byte, error) {
 		// Validate byte range
 		for _, b := range payload {
 			if b < 0x20 || b > 0x7f {
-				return 0, nil, fmt.Errorf("protocol violation. expecting chars in the range 0x20-0x7f but received %x", data[0])
+				return 0, nil, ErrBytesOutOfRange
 			}
 		}
 
-		crc := data[i+2 : i+4]
+		crc := data[i+1 : i+1+2]
 
 		//Validate CRC (bytes must be reversed)
-		crcErr := crc16.Validate(data[1:i+2], []byte{crc[1], crc[0]})
+		crcErr := crc16.Validate(data[1:i+1], []byte{crc[1], crc[0]})
 
 		// We have a full terminated line.
 		return totalLength, payload, crcErr
--- lib/raw/scanner_test.go
+++ lib/raw/scanner_test.go
@@ -2,9 +2,12 @@ package raw
 
 import (
 	"bufio"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/mniak/ppabecs/lib/utils"
+	"github.com/stellar/go/crc16"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -73,4 +76,92 @@ func TestScanWellFormattedMessage_WithCANInTheBeginning(t *testing.T) {
 			assert.Equal(t, d.text, text, "scan text is invalid")
 		})
 	}
+}
+
+func TestScanWithWrongCRC(t *testing.T) {
+
+	alice, bob := EntangledReadWriters()
+
+	scanner := bufio.NewScanner(alice)
+	scanner.Split(PayloadScanner)
+
+	bytes := utils.NewBytesBuilder().
+		AddByte(SYN).
+		AddString("ABCDEFG").
+		AddByte(ETB).
+		AddByte(0x11, 0x22).
+		Bytes()
+
+	go bob.Write(bytes)
+	assert.False(t, scanner.Scan(), "scan should fail")
+	assert.Error(t, scanner.Err(), "scan should raise error")
+	assert.Equal(t, scanner.Err(), crc16.ErrInvalidChecksum, "scan error should be due to CRC")
+}
+
+func TestScanWithByteOutOfRange(t *testing.T) {
+	testData := []byte{
+		0x00, 0x11, 0x19,
+		0x90, 0xa0, 0xf0,
+	}
+	for _, b := range testData {
+		t.Run(fmt.Sprintf("%x", b), func(t *testing.T) {
+			alice, bob := EntangledReadWriters()
+
+			scanner := bufio.NewScanner(alice)
+			scanner.Split(PayloadScanner)
+
+			bytes := utils.NewBytesBuilder().
+				AddByte(SYN).
+				AddString("ABCD").
+				AddByte(b).
+				AddString("EFGH").
+				AddByte(ETB).
+				AddByte(0x11, 0x22).
+				Bytes()
+
+			go bob.Write(bytes)
+			assert.False(t, scanner.Scan(), "scan should fail")
+			assert.Error(t, scanner.Err(), "scan should raise error")
+			assert.Equal(t, scanner.Err(), ErrBytesOutOfRange, "scan error should be due to bytes out of range")
+		})
+	}
+}
+
+func TestScanWithPayloadLength0(t *testing.T) {
+
+	alice, bob := EntangledReadWriters()
+
+	scanner := bufio.NewScanner(alice)
+	scanner.Split(PayloadScanner)
+
+	bytes := utils.NewBytesBuilder().
+		AddByte(SYN).
+		AddByte(ETB).
+		AddByte(0x11, 0x22).
+		Bytes()
+
+	go bob.Write(bytes)
+	assert.False(t, scanner.Scan(), "scan should fail")
+	assert.Error(t, scanner.Err(), "scan should raise error")
+	assert.Equal(t, scanner.Err(), ErrMessageTooShort, "scan error should be due to payload too short")
+}
+
+func TestScanWithPayloadLengthGreaterThan1024(t *testing.T) {
+
+	alice, bob := EntangledReadWriters()
+
+	scanner := bufio.NewScanner(alice)
+	scanner.Split(PayloadScanner)
+
+	bytes := utils.NewBytesBuilder().
+		AddByte(SYN).
+		AddString(strings.Repeat("a", 1024+1)).
+		AddByte(ETB).
+		AddByte(0x11, 0x22).
+		Bytes()
+
+	go bob.Write(bytes)
+	assert.False(t, scanner.Scan(), "scan should fail")
+	assert.Error(t, scanner.Err(), "scan should raise error")
+	assert.Equal(t, scanner.Err(), ErrMessageTooLong, "scan error should be due to payload too long")
 }
